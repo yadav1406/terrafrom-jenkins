31c6dd586960a544f2ee11fb2add16b713d3a479
Akash_Yadav <yadavak1406@gmail.com>
2026-03-25 16:29:32 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id = "ami-02dfbd4ff395f2a1b"
+instance_type = "t3.micro"
+key_name = "akash-key"
---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable "ami_id" {
+    default = "ami-02dfbd4ff395f2a1b"
+    description = "this is ami id "
+
+  
+}
+variable "instance_type" {
+    default = "t3.micro"
+    description = "this is instance type"
+
+  
+}
+variable "key_name" {
+    default = "akash-key"
+    description = "this is key name"
+
+  
+}
